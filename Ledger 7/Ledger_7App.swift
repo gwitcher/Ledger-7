50362a3f9eab51b85a68f01554a7aa6d6c2f5fb8
Gabe Witcher <gabewitcher@gmail.com>
2025-03-27 12:32:42 -0700

    Initial Commit

---
+++ Ledger 7/Ledger_7App.swift
@@ -0,0 +1,17 @@
+//
+//  Ledger_7App.swift
+//  Ledger 7
+//
+//  Created by Gabe Witcher on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Ledger_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
